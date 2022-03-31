cc2221052098f1ba78a236df0c5555276854c349
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-03-31 15:30:55 -0300

    base-project

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+/* SLIDER */
+const foodSlider = tns({
+    container: '#food-options',
+    items: 2,
+    controls: false,
+    mouseDrag: true,
+    touch: true,
+    loop: false,
+    nav: false,
+});
+
+const drinkSlider = tns({
+    container: '#drink-options',
+    items: 2,
+    controls: false,
+    mouseDrag: true,
+    touch: true,
+    loop: false,
+    nav: false,
+});
+
+const dessertSlider = tns({
+    container: '#dessert-options',
+    items: 2,
+    controls: false,
+    mouseDrag: true,
+    touch: true,
+    loop: false,
+    nav: false,
+});
